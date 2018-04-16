f06f13ab540cc80c02c5ce8b150da1dd392ed05d
Hendi Santika <hendisantika@gmail.com>
2018-04-17 06:18:21 +0700

    #3 - Add Account Model

---
+++ src/main/java/com/hendisantika/jwt/model/Account.java
@@ -0,0 +1,60 @@
+package com.hendisantika.jwt.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-jwt-rest-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/04/18
+ * Time: 06.17
+ * To change this template use File | Settings | File Templates.
+ */
+public class Account {
+    private String id;
+    private String username;
+    private String password;
+    private boolean enabled;
+
+    public Account() {
+    }
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.enabled = true;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+}
