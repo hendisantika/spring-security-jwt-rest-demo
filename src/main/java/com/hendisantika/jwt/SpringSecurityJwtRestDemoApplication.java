87fb342962ed82542ef909ca5c039b6249e9dd58
Hendi Santika <hendisantika@gmail.com>
2018-04-17 06:12:26 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/jwt/SpringSecurityJwtRestDemoApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtRestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtRestDemoApplication.class, args);
+    }
+}
